3800467eba5ee8d086ae06e87f5dc481ba55da80
IhoraDev <Ihor.Lukachuk.Dev@gmail.com>
2020-04-03 01:08:13 +0300

    Updates

---
+++ precache-manifest.61a689eb07279a05ff83835829f52913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d75a05444565bcbe250cc924a500307",
+    "url": "/Pokemons/index.html"
+  },
+  {
+    "revision": "236085b6cd9553dcf1f9",
+    "url": "/Pokemons/static/css/main.6f1a0569.chunk.css"
+  },
+  {
+    "revision": "cf00e159e6a6e53dce65",
+    "url": "/Pokemons/static/js/2.d1d7b814.chunk.js"
+  },
+  {
+    "revision": "236085b6cd9553dcf1f9",
+    "url": "/Pokemons/static/js/main.33b36e49.chunk.js"
+  },
+  {
+    "revision": "4b6f498a1bdde8e9effc",
+    "url": "/Pokemons/static/js/runtime~main.0342fecb.js"
+  },
+  {
+    "revision": "605ebff03a482f2afbeedfad48b8cb4b",
+    "url": "/Pokemons/static/media/Pokemon_logo.605ebff0.svg"
+  }
+]);
